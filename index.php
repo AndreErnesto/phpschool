712ab9c5637f4bd8c76fb69c0253166fbb0cf346
AndreErnesto <woodlemax@sapo.pt>
2020-11-16 18:08:53 +0000

    Update

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>School - PHP</title>
+</head>
+<body>
+
+<h1> School - PHP</h1>
+<p><?php
+        echo("Hoje é " .date("Y/m/d"). "<br>");
+        ?>
+</p>
+
+<h1>Part II - Learning the language</h1>
+
+<ul>
+    <li><a href="#">PHP Language Basics</a></li>
+    <li><a href="#">Decisions and Loops</a></li>
+    <li><a href="#">Strings</a></li>
+    <li><a href="#">Arrays</a></li>
+    <li><a href="#">Arrays </a></li>
+    <li><a href="#">Functions</a></li>
+    <li><a href="#">Objects</a></li>
+</ul>
+
+</body>
+</html>
