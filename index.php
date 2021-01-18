1345bf001f7eb208aeb406b78b3c869dfc0788c2
AndreErnesto <woodlemax@sapo.pt>
2021-01-18 17:29:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <h1>Part II - Learning the language</h1>
 
-<ul>
+<!-- <ul>
     <li><a href="challenge1/Challenge1.php">Challenge 1 </a>
     <li><a href="challenge02/Challenge02.php">Challenge 2 </a>
     <li><a href="php/c1_PHPLanguageBasics.php">PHP Language Basics</a></li>
@@ -26,7 +26,45 @@
     <li><a href="php/c12_PHPFormValidation.php">Form Validation </a></li>
     <li><a href="#">Functions</a></li>
     <li><a href="#">Objects</a></li>
-</ul>
+</ul> -->
+<ul>
+        <li><a href="php/c1_PHPLanguageBasics.php">PHP Language Basics</a></li>
+        <li><a href="php/c2_PHPDecisionsAndLoops.php">Decisions and Loops</a></li>
+        <li><a href="php/c3_PHPFunctions.php">Functions</a></li>
+        <li><a href="php/c4_PHPArrays.php">Arrays</a></li>
+        <li><a href="php/c5_PHPSuperglobals.php">Superglobals</a></li>
+        <li><a href="challenge1/Challenge1.php">Challenge01</a></li>
+    </ul>
+    <br>
+    <h1>Part III – PHP Forms</h1>
+    <ul>
+        <li><a href="php/c11_PHPFormHandling.php">PHP Form Handling</a></li>
+        <li><a href="php/c12_PHPFormValidation.php">PHP Form Validation</a></li>
+        <li><a href="php/c13_PHPRequiredFields.php">PHP Forms - Required Fields</a></li>
+        <li><a href="php/c14_PHPValidateEmail_URL.php">PHP Forms - Validate E-mail and URL</a></li>
+        <li><a href="php/c15_PHPCompleteFormExample.php">PHP Complete Form Example</a></li>
+        <li><a href="/auxStuff/challenge02/challenge02.php">Challenge02</a></li>
+    </ul>
+
+    <br>
+
+    <br>
+    <h1>Part VI – MySQL DB</h1>
+    <ul>
+        <li><a href="/php/c41_PHPOpenCloseConnection1.php">Open Connection 1</a></li>
+        <li><a href="/php/c41_PHPOpenCloseConnection2.php">Open Connection 2</a></li>
+        <li><a href="/php/c42_PHPreadRecordWithPHP.php">Read a record with PHP</a></li>
+        <li><a href="/php/c43_PHPAddRecord.php">Add a record to a MySQL database</a></li>
+        <li><a href="/php/testPrep.php">Prepared Statements</a></li>
+        <li><a href="/php/testPrep2.php">INSERT with Prepared Statements</a></li>
+        <li><a href="/php/testPrep3.php">UPDATE with Prepared Statements</a></li>
+        <li><a href="/php/testPrep4.php">DELETE with Prepared Statements</a></li>
+
+        <li><a href="/login/signup.php">Sign up</a></li>
+        <li><a href="/login/login.php">login</a></li>
+        
+    </ul>
+
 
 </body>
 </html>
